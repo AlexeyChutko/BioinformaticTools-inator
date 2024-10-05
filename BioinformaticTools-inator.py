c5c999ad93a5d8f0975f90ba30bcf36822d50183
alexey <alexey@Main.localdomain>
2024-10-05 17:04:01 +0300

    Add flake8 imporvements

--- BioinformaticTools-inator.py
+++ BioinformaticTools-inator.py
@@ -9,14 +9,17 @@ def run_dna_rna_tools(
     Performs a specified action on all sequences transmitted from input
 
     Args:
-    - DNA or RNA sequences as str and a procedure to perform as the last argument
+    - DNA or RNA sequences as str and a procedure to perform
+      as the last argument
 
     Possible procedures:
     - Main procedures: reverse, transcribe, complement, reverse_complement
-    - Additional procedures: nucleotide_frequency, gc_content, find_start_codons
+    - Additional procedures:
+      nucleotide_frequency, gc_content, find_start_codons
 
     Returns:
-    - Union[List[str], Dict[str, Dict[str, int]], str]: sequence or a collection of sequences with paramters
+    - Union[List[str], Dict[str, Dict[str, int]], str]:
+      sequence or a collection of sequences with paramters
     """
     for j in args[:-1]:
         if ('u' in j or 'U' in j) and ('t' in j or 'T' in j):
@@ -50,15 +53,21 @@ def filter_fastq(
         quality_threshold: int = 0
         ) -> Dict[str, Tuple[str, str]]:
     """
-    Filters provided sequences based on its gc composition, length, and average sequence quality
+    Filters provided sequences based on its gc composition, length
+    and average sequence quality
 
     Args:
-    - seqs Dict[str, Tuple[str, str]]: a dictionary consisting of fastq sequents.
+    - seqs Dict[str, Tuple[str, str]]: a dictionary consisting of
+      fastq sequents.
         The key is a string, the name of the sequence.
         The value is a tuple of two strings: sequence and quality.
-    - gc_bounds Union[Tuple[float, float], float]: the GC interval of the composition (in percent) for filtration
-    - length_bounds Union[Tuple[int, int], int]: the length interval for filtering within which the filtered sequences should be
-    - quality_threshold int: the threshold value of the average read quality for filtering
+    - gc_bounds Union[Tuple[float, float], float]:
+      the GC interval of the composition (in percent) for filtration
+    - length_bounds Union[Tuple[int, int], int]:
+      the length interval for filtering
+      within which the filtered sequences should be
+    - quality_threshold int:
+      the threshold value of the average read quality for filtering
 
     Returns:
     - Dict[str, Tuple[str, str]]: a dictionary with filtered sequences
